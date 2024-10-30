b14f5c6465ce7099991a698f965fc22c52c9b758
Dipen Kumar <dipenbachani@gmail.com>
2024-10-29 22:41:38 -0600

    Completed signup fucntionality and changed xml files

--- app/src/main/java/com/example/ocelotnovels/MainActivity.java
+++ app/src/main/java/com/example/ocelotnovels/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.ocelotnovels;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +10,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.Firebase;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    TextView textView;
+    FirebaseUser user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +32,17 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        mAuth = FirebaseAuth.getInstance();
+        textView = findViewById(R.id.test);
+        user = mAuth.getCurrentUser();
+
+        if (user == null){
+            Intent intent = new Intent(getApplicationContext(), SignUpActivity.class);
+            startActivity(intent);
+            finish();
+        } else {
+            textView.setText(user.getEmail() + user.getUid());
+        }
     }
 }
