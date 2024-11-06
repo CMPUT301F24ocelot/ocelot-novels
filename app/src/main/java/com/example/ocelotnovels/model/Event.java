d3d7ceeca678874d54f3faf1a38ca0ec13403c99
CubeMeister3D <146042583+CubeMeister3D@users.noreply.github.com>
2024-11-05 21:51:32 -0700

    added  functions to add and delete entrants from waitlist and xml for entrant to view their events they are enrolled in

--- app/src/main/java/com/example/ocelotnovels/model/Event.java
+++ app/src/main/java/com/example/ocelotnovels/model/Event.java
@@ -1,4 +1,16 @@
 package com.example.ocelotnovels.model;
 
+import java.util.ArrayList;
+
 public class Event {
+    ArrayList<Entrant> waitList = new ArrayList<Entrant>();
+
+    public void addEntrantToWaitList(Entrant user){
+        if(!waitList.contains(user)){
+            waitList.add(user);
+        }
+    }
+    public void removeEntrantFromWaitList(Entrant user){
+        waitList.remove(user);
+    }
 }
