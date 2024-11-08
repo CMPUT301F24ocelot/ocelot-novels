a075a4d78bde939885112a2d9c9b8ff82e0e3b59
Akshay Krishna <akshaydkrishna12@gmail.com>
2024-11-08 15:34:39 -0700

    fixing up minor changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -77,7 +77,7 @@ dependencies {
     implementation("com.google.android.gms:play-services-base:18.5.0")
     implementation("com.google.android.gms:play-services-tflite-java:16.2.0-beta02")
     implementation("com.google.android.gms:play-services-maps:19.0.0")
-
+    implementation("com.journeyapps:zxing-android-embedded:4.1.0")
     testImplementation("junit:junit:4.13.2")
     testImplementation("org.mockito:mockito-core:4.5.1")
     testImplementation("androidx.test.ext:junit:1.1.3")
